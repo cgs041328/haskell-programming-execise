3e1e6b7c52fe7fac6983800606a635677e0f00c6
cgs041328 <cgs931219@outlook.com>
2018-05-16 23:13:28 +0800

    wordnumber

---
+++ wordnumber/src/Lib.hs
@@ -0,0 +1,29 @@
+module Lib
+    ( digitToWord, digits, wordNumber
+    ) where
+
+import Data.List(intersperse)
+
+digitToWord :: Int -> String
+digitToWord n =
+    case n of
+        0 -> "zero"
+        1 -> "one"
+        2 -> "two"
+        3 -> "three"
+        4 -> "four"
+        5 -> "five"
+        6 -> "six"
+        7 -> "seven"
+        8 -> "eight"
+        9 -> "nine"
+
+digits :: Int -> [Int]
+digits n = go n []
+    where go a xs
+            | a `div` 10 > 0 = go (a `div` 10) (a `mod` 10 : xs )
+            | otherwise      = a : xs
+
+wordNumber :: Int -> String
+wordNumber n = concat $ intersperse "-" $ map digitToWord $ digits n
+
