86b2d4d1af9b3e8563e756670e472e729b880d88
cgs041328 <cgs931219@outlook.com>
2018-07-25 22:43:50 +0800

    ch17 exercises

---
+++ ch17-exercises.hs
@@ -0,0 +1,10 @@
+import Control.Applicative (liftA3)
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a, b, c)]
+combos as bs cs = liftA3 (,,) as bs cs
