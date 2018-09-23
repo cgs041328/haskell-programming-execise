5f11d0adf62b8917c760eef43a73561f9855a75a
cgs041328 <cgs931219@outlook.com>
2018-09-23 13:45:59 +0800

    hit-counter

---
+++ hit-counter.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Reader
+import           Data.IORef
+import qualified Data.Map                   as M
+import           Data.Maybe                 (fromMaybe)
+import           Data.Text.Lazy             (Text)
+import qualified Data.Text.Lazy             as TL
+import           System.Environment         (getArgs)
+import           Web.Scotty.Trans
+
+data Config =
+    Config {
+    -- that's one, one click!
+    -- two...two clicks!
+    -- Three BEAUTIFUL clicks! ah ah ahhhh
+    counts :: IORef (M.Map Text Integer)
+    , prefix :: Text
+    }
+
+type Scotty = ScottyT Text (ReaderT Config IO)
+type Handler = ActionT Text (ReaderT Config IO)
+
+bumpBoomp :: Text -> M.Map Text Integer -> (M.Map Text Integer, Integer)
+bumpBoomp text map = (M.insert text bumped map, bumped)
+  where bumped = 1 + (fromMaybe 0 $ M.lookup text map)
+
+app :: Scotty ()
+app = 
+    get "/:key" $ do
+        unprefixed <- param "key"
+        config <- lift ask
+        let key' = mappend (prefix config) unprefixed
+            mapRef = counts config
+            map = readIORef mapRef
+        (newMap, newInteger) <- liftIO $ bumpBoomp key' <$> map
+        liftIO $ writeIORef mapRef newMap
+        html $
+            mconcat [ "<h1>Success! Count was: "
+                    , TL.pack $ show newInteger
+                    , "</h1>"
+                    ]
+
+main :: IO ()
+main = do
+    [prefixArg] <- getArgs
+    counter <- newIORef M.empty
+    let config = Config counter (TL.pack prefixArg)
+        runR a = runReaderT a config
+    scottyT 3000 runR app
